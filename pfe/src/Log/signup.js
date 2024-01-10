17f7e8878e4e54b4331d73c968b2574e3acfac1c
Ali-Dehissy <97989740+Ali-Dehissy@users.noreply.github.com>
2024-01-11 00:04:59 +0100

    update

--- pfe/src/Log/signup.js
+++ pfe/src/Log/signup.js
@@ -5,40 +5,37 @@ import background from '../image/background.jpg';
 
 function signup() {
 	return (
-			<div className="signup">
-				<div className='signup'>
-				<div className='App-image'>
-      <img className='App-logo' src={logo} alt='logo'></img>
-						<img className='background-image' scr={background} alt='background'></img>
-					</div>
-					<div id="top">
-            <h1 id="home">Home</h1>
-            <h1 id="about">About</h1>
-            <h1 id="travel">Travel</h1>
-            <h1 id="contact">Contact</h1>
-        </div>
-        <div class="signup-box">
-            <h1>Sign up</h1>
-            <h4>It's free !</h4>
-            <form>
-              <label>First Name</label>
-              <input type="text"/>
-              <label>Last Name</label>
-              <input type="text"/>
-              <label>Email</label>
-              <input type="email"/>
-              <label>Password</label>
-              <input type="password"/>
-              <label>Confirm Password</label>
-              <input type="password"/>
-              <input type="button" value="Submit" />
-              <p class="p1">Already have an account ?<a href="signin.html"> Click Here</a></p>
-            </form>
-          </div>
-										</div> 
-        </div>
-			
-	);
-}
+<div id="top">
+    <img className="logo" src="./logo.jpg" />
+    <h1 className="home">Home</h1>
+    <h1 className="about">About</h1>
+    <h1 className="contact">Contact</h1>
+    <h1 className="signin">
+      <a href="signin.html">
+        <img src="signin.jpg" className="imgsignin" />
+        Sign In
+      </a>
+    </h1>
+  </div>
+  <div className="signup-box">
+    <h1>Sign up</h1>
+    <h4>It's free !</h4>
+    <form>
+      <label>First Name</label>
+      <input type="text" placeholder="" />
+      <label>Last Name</label>
+      <input type="text" placeholder="" />
+      <label>Email</label>
+      <input type="email" placeholder="" />
+      <label>Password</label>
+      <input type="password" placeholder="" />
+      <label>Confirm Password</label>
+      <input type="password" placeholder="" />
+      <input type="button" defaultValue="Submit" />
+      <p className="p1">
+        Already have an account ?<a href="signin.html"> Click Here</a>
+      </p>
+    </form>
+  </div>
 
 export default signup;
