52faefa66511af4853ca588403a8323d1edce784
Ali-Dehissy <97989740+Ali-Dehissy@users.noreply.github.com>
2024-01-11 00:19:17 +0100

    Update

--- pfe/src/App.js
+++ pfe/src/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import './App.css';
 import Header from './Header/Header';
-import Login from './Log/Login';
+import Login from './Log/signin';
 import Logout from './Log/signup';
 
 function App() {
