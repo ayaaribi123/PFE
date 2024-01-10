346a693e5b0c964c0535fd1cad2db0b6f9d5f0a8
Ali-Dehissy <97989740+Ali-Dehissy@users.noreply.github.com>
2024-01-11 00:58:07 +0100

    .

--- pfe/src/Header/Header.js
+++ pfe/src/Header/Header.js
@@ -5,13 +5,19 @@ import background from '../image/background.jpg';
 
 export default function Header() {
   return (
-    <div className='App-header'>
-      <img className='App-logo' src={logo} alt='logo'></img>
-						<img className='background-image' scr={background} alt='background'></img>
-					<h1 class="home">Home</h1>
-					<h1 class="about">About</h1>
-					<h1 class="travel">Travel</h1>
-					<h1 class="contact">Contact</h1>
+	<div className='App-header'>
+		<img className='App-logo' src={logo} alt='logo'></img>
+		<img className='background-image' scr={background} alt='background'></img>
+			<h1 class="home">Home</h1>
+			<h1 class="about">About</h1>
+			<h1 class="travel">Travel</h1>
+			<h1 class="contact">Contact</h1>
+			<h1 className="signin">
+			<a href="signin.html">
+		<img src="signin.jpg" alt="Sign In" className="imgsignin" />
+              Sign In
+            </a>
+          </h1>
 				</div>
   );
 }
