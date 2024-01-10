7e7ce3e28db3d0452948445fbc55f40aac2db604
Ali-Dehissy <97989740+Ali-Dehissy@users.noreply.github.com>
2024-01-11 00:26:39 +0100

    .

--- pfe/src/Log/signin.js
+++ pfe/src/Log/signin.js
@@ -5,30 +5,38 @@ import background from '../image/background.jpg';
 
 function signin() {
 	return (
-<div>
+<html>
+      <head>
+        <link rel="stylesheet" href="styles.css" />
+      </head>
+      <body style={{ backgroundImage: 'url(background.jpg)' }} className="bg">
         <div id="top">
-          <img className="logo" src="./logo.jpg" />
+          <img className="logo" src="./logo.jpg" alt="Logo" />
           <h1 className="home">Home</h1>
           <h1 className="about">About</h1>
           <h1 className="contact">Contact</h1>
-          <h1 className="signin"><a href="signin.html"><img src="signin.jpg" className="imgsignin" />Sign In</a></h1>
+          <h1 className="signin">
+            <a href="signin.html">
+              <img src="signin.jpg" alt="Sign In" className="imgsignin" />
+              Sign In
+            </a>
+          </h1>
         </div>
         <div className="login-box">
           <h1>Sign in</h1>
           <form>
             <label>Email</label>
-            <input type="email" placeholder />
+            <input type="email" placeholder="" />
             <label>Password</label>
-            <input type="password" placeholder />
-            <input type="button" defaultValue="Submit" />
+            <input type="password" placeholder="" />
+            <input type="button" value="Submit" />
             <p className="p2">
               Not having an account? <a href="signup.html">Sign Up Here</a>
             </p>
           </form>
         </div>
-      </div>
-    );
-  }
-);
-
+      </body>
+    </html>
+  );
+};
 export default signin;
