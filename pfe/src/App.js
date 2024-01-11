7d0329fad682574fb56effe6d86d0d1fdeeea366
Ali-Dehissy <97989740+Ali-Dehissy@users.noreply.github.com>
2024-01-11 01:17:08 +0100

    .

--- pfe/src/App.js
+++ pfe/src/App.js
@@ -4,12 +4,15 @@ import './App.css';
 import Header from './Header/Header';
 import signin from './Log/signin';
 import signup from './Log/signup';
+import Footer from './log/footer';
 
 function App() {
   return (
     <React.Fragment>
       <div className='App'>
         <Header />
+    <div className='App'>
+        <Footer />
       </div>
       <div className='App-body'>
         <signin />
