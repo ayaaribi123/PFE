b27a380cd925cf229bcf966ab925f4b8cf63bd7d
ayaaribi <93769004+ayaaribi123@users.noreply.github.com>
2024-01-11 11:02:32 +0100

    pfe

--- pfe/src/Log/auth.js
+++ pfe/src/Log/auth.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const User = require("../mongolog/User");
 const CryptoJS = require("crypto-js");
-const jwt = require("jsonwebtoken");
 
 //REGISTER
 
@@ -35,24 +34,10 @@ router.post("/login", async (req, res) => {
 	try {
 		const user = await User.findOne({ username: req.body.username });
 		!user && res.status(401).json("Wrong User Name !");
-
-		const hashedPassword = CryptoJS.AES.decrypt(
-			user.password,
-			process.env.PASS_SEC
-		);
 		const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
 
 		OriginalPassword !== req.body.password &&
 			res.status(401).json("Wrong password !");
-
-		const accessToken = jwt.sign(
-			{
-				id: user._id,
-				isAdmin: user.isAdmin,
-			},
-			process.env.JWT_SEC,
-			{ expiresIn: "3d" }
-		);
 
 		/*
 		// To not show the password when user informations show up*/
