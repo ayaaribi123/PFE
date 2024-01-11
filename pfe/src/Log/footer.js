f04e6fd833e9001806af8f4e52ea3559bf3dd444
Ali-Dehissy <97989740+Ali-Dehissy@users.noreply.github.com>
2024-01-11 01:16:13 +0100

    .

---
+++ pfe/src/Log/footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../App.css';
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="footercontainer">
+        <div className="icons">    
+          <a href="https://www.facebook.com"><i className="fa-brands fa-facebook"></i></a>
+          <a href="https://www.instagram.com"><i className="fa-brands fa-instagram"></i></a>
+          <a href="https://twitter.com"><i className="fa-brands fa-twitter"></i></a>
+          <a href="https://www.youtube.com"><i className="fa-brands fa-youtube"></i></a>
+        </div>
+      </div>
+      <div className="footerbottom">
+        <p>Copyright &copy;2024 Sky High</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
