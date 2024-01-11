0f7d68069bc126911f5e1071117ca3e1c449828d
ayaaribi <93769004+ayaaribi123@users.noreply.github.com>
2024-01-11 10:42:15 +0100

    pfe

--- pfe/src/Log/admin.js
+++ pfe/src/Log/admin.js
@@ -1,17 +1,1 @@
-const {
-	verifyToken,
-	verifyTokenAndAuthorization,
-	verifyTokenAndAdmin,
-} = require("./verifyToken");
 
-const router = require("express").Router();
-
-router.get("/", (req, res) => {
-	try {
-		res.render("admin");
-	} catch (err) {
-		res.redirect("/auth/login");
-	}
-});
-
-module.exports = router;
